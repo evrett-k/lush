37eae6c684944507c32b63f8052cfb3aa0b2a026
evrett-k <everett.kamulda@outlook.com>
2026-06-16 15:54:32 -1000

    fix: use apple-sdk-frameworks for darwin buildInputs

--- flake.nix
+++ flake.nix
@@ -37,8 +37,8 @@
             openssl
             libiconv
           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            pkgs.apple-sdk-frameworks.Security
+            pkgs.apple-sdk-frameworks.SystemConfiguration
           ];
 
           checkFlags = [ "--skip=repl" ];
@@ -72,8 +72,8 @@
             nfpm
             docker
           ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin [
-            pkgs.darwin.apple_sdk.frameworks.Security
-            pkgs.darwin.apple_sdk.frameworks.SystemConfiguration
+            pkgs.apple-sdk-frameworks.Security
+            pkgs.apple-sdk-frameworks.SystemConfiguration
           ] ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
             pkgs.glibc
           ];
